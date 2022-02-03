ab76161a57121fe59b5c089470af89b49563ba2d
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2022-02-02 21:29:32 -0300

    Feita criacao do banco de dados via sql e por sqlalchemy; Criado model Hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -36,7 +37,6 @@ class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
 
-
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
     argumentos.add_argument('nome')
@@ -58,18 +58,14 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = self.argumentos.parse_args()
-
-        novo_hotel = {
-            'hotel_id': hotel_id, **dados
-        }
-
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
 
     def put(self, hotel_id):
         dados = self.argumentos.parse_args()
-        novo_hotel = {
-            'hotel_id': hotel_id, **dados
-        }
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = self.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
